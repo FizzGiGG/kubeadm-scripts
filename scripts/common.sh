e0d6a8801a9062e4a573e854b7dd04e5c61a74d6
Jesse Beauclaire <1538645+FizzGiGG@users.noreply.github.com>
2026-02-20 15:40:37 -0600

    added sudo apt-get update pre containerd.io install

--- scripts/common.sh
+++ scripts/common.sh
@@ -53,7 +53,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install containerd.io
+sudo apt-get update -y
+sudo apt-get install containerd.io -y
 
 sudo systemctl daemon-reload
 sudo systemctl enable containerd --now
