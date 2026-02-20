3ed9d33f645dd492ee08eb09c3b403d63fcdc056
Jesse Beauclaire <1538645+FizzGiGG@users.noreply.github.com>
2026-02-20 16:14:15 -0600

    add back another apt-get update -y

--- scripts/common.sh
+++ scripts/common.sh
@@ -106,6 +106,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
     sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+sudo apt-get update -y
 sudo apt-get install -y kubelet="$KUBERNETES_INSTALL_VERSION" kubectl="$KUBERNETES_INSTALL_VERSION" kubeadm="$KUBERNETES_INSTALL_VERSION"
 
 # Prevent automatic updates for kubelet, kubeadm, and kubectl
